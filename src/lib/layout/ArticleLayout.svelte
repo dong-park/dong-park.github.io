1658bc6b4a360df5b6cef0cea8cc4a0f1fbe7773
dong-park <rakddh@gmail.com>
2024-05-10 19:22:45 +0900

    fix: 스크롤이벤트 테스트

--- src/lib/layout/ArticleLayout.svelte
+++ src/lib/layout/ArticleLayout.svelte
@@ -23,8 +23,8 @@
 	<meta property="og:site_name" content="dongpark.land" />
 </svelte:head>
 
-<main bind:this={element}  class="overflow-scroll main-height">
-	<article class="relative bg-white pt-2 px-4" data-animate data-animate-speed="slow">
+<main class="overflow-scroll main-height">
+	<article bind:this={element} class="relative bg-white pt-2 px-4" data-animate data-animate-speed="slow">
 		<h1>{title}</h1>
 		<div class="text-sm">{transformDate(date)}</div>
 		<IndexNavigationBar />
@@ -43,6 +43,7 @@
     width: 100%;
     max-width: var(--content-width);
   }
+
   .main-height {
     height: calc(100vh - 52px);
   }
