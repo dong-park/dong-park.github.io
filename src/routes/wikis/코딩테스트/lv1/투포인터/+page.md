31ba31ada88410d8cf6cefd34df070167199e23f
dong-park <rakddh@gmail.com>
2024-05-25 01:47:27 +0900

    add: 투포인터 추가

---
+++ src/routes/wikis/코딩테스트/lv1/투포인터/+page.md
@@ -0,0 +1,20 @@
+---
+title: 투포인터
+summary: ""
+date: '2024-05-25'
+tags: [ '' ]
+---
+
+# 투포인터 기법
+
+startIndex, endIndex를 지정해서 한칸씩 이동시키며 원하는 합이나 수들을 찾는 방법이다.
+아래 이동원칙을 endIndex가 N이 될 때 까지 반복하되, 포인터가 이동할 때마다 현재의 총합과 비교해가며 값을 채운다.
+
+## 투포인터 이동 원칙
+
+- sum > N : sum = sum - startIndex; startIndex++;
+- sum < N: endIndex++; sum = sum + endIndex;
+- sum == N: endIndex++; sum = sum + endIndex; count++;
+
+
+
---
+++ src/routes/wikis/코딩테스트/lv1/투포인터/Example1.java
@@ -0,0 +1,38 @@
+package wikis.코딩테스트.lv1.투포인터;
+
+public class Example1 {
+
+    // 어떠한 자연수 N은 몇 개의 연속된 자연수의 합으로 나타낼 수 있다.
+    // 당신은 어떤 자연수 N(1 ≤ N S 10,000,000)을 몇 개의 연속된 자연수의 합으로 나타내는 가짓수를 알고 싶다. 이때 사 용하는 자연수는 N이어야 한다.
+    // 예를 들어 15를 나타내는 방법은 15,7+8,4+5+ 6,1+2 + 3+ 4+ 5이다.
+    // 반면, 10을 나타내는 방법은 10,1+ 2+ 3+ 4이다.
+    // N을 입력받아 연속된 자 연수의 합으로 나타내는 가짓수를 출력하는 프로그램을 작성하시오
+    public static void main(String[] args) {
+        // N 변수 초기화
+        long N = 15;
+        // 사용 변수 초기화
+        long cnt = 1, sum = 1, sI = 1, eI = 1;
+        // while ( N != eI) {
+        // if(sum == N) -> cnt++, eI++, sum=sum+eI
+        // else if(sum > N) -> 수가 크다, sum - sI, sI++
+        // else -> 수가 작다, eI++ , sum += eI
+        // }
+        // cnt 출력
+        while (N > eI) {
+            if (sum == N) {
+                cnt++;
+                eI++;
+                sum += eI;
+            } else if(sum >  N) {
+                sum -= sI;
+                sI++;
+            } else {
+                eI++;
+                sum += eI;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+}
